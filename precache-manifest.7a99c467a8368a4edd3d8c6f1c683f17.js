b5a7a94c2c218ccfa029d24d4be7ff66f42f1f58
Abhik8261 <abhik.tech8261@gmail.com>
2022-09-18 21:44:02 +0530

    Updates

---
+++ precache-manifest.7a99c467a8368a4edd3d8c6f1c683f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89a4607b10aa7bdef4eb",
+    "url": "/calculator--react/static/css/main.b20249ea.chunk.css"
+  },
+  {
+    "revision": "89a4607b10aa7bdef4eb",
+    "url": "/calculator--react/static/js/main.89a4607b.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/calculator--react/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "fc6bf2a49d77b21506bb",
+    "url": "/calculator--react/static/js/2.fc6bf2a4.chunk.js"
+  },
+  {
+    "revision": "0ae6b30a16b6d4201a29",
+    "url": "/calculator--react/static/js/runtime~main.0ae6b30a.js"
+  },
+  {
+    "revision": "d3b3613be7763435a896cec1c7566fd7",
+    "url": "/calculator--react/index.html"
+  }
+];
